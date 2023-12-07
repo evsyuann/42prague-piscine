a9923c2d4ce91f416332dc3e0640adc1243025a2
evsyuann <146770032+evsyuann@users.noreply.github.com>
2023-12-07 13:59:01 +0100

    Add files via upload

--- c09/ex00/libft_creator.sh
+++ c09/ex00/libft_creator.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 rm -f libft.a
 find . -name "*.c" -type f -exec gcc -Wall -Wextra -Werror -c {} \;
 ar rc libft.a *.o
--- c09/ex02/ft_split.c
+++ c09/ex02/ft_split.c
@@ -6,66 +6,106 @@
 /*   By: aevsyuti <aevsyuti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 13:17:24 by aevsyuti          #+#    #+#             */
-/*   Updated: 2023/12/06 14:48:00 by aevsyuti         ###   ########.fr       */
+/*   Updated: 2023/12/07 12:35:35 by aevsyuti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <stdlib.h>
 #include <stdio.h>
 
-char	*ft_strstr(char *str, char *to_find)
+int	is_sep(char c, char *charset)
 {
-	unsigned int	i;
-	unsigned int	j;
+	int	i;
 
 	i = 0;
-	j = 0;
-	if (to_find[0] == '\0')
-		return (str);
-	while (str[i] != '\0')
+	while (charset[i] != '\0')
 	{
-		if (str[i] == to_find[j])
-		{
-			if (to_find[j + 1] == '\0')
-				return (str + i + 1);
-			++j;
-		}
-		else
-			j = 0;
+		if (c == charset[i])
+			return (1);
 		++i;
 	}
+	if (c == '\0')
+		return (1);
 	return (0);
+}
+
+int	count_words_for_mall(char *str, char *charset)
+{
+	int	i;
+	int	count;
+
+	count = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (is_sep(str[i], charset) == 0 && is_sep(str[i + 1], charset) == 1)
+			++count;
+		++i;
+	}
+	return (count);
+}
+
+void	copystr(char *dest, char *src, char *charset)
+{
+	int		i;
+
+	i = 0;
+	while (is_sep(src[i], charset) == 0)
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = '\0';
+}
+
+void	split(char **array, char *str, char *charset)
+{
+	int	i;
+	int	j;
+	int	word;
+
+	word = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (is_sep(str[i], charset) == 1)
+			++i;
+		else
+		{
+			j = 0;
+			while (is_sep(str[i + j], charset) == 0)
+				j++;
+			array[word] = (char *)malloc(sizeof(char) * (j + 1));
+			copystr(array[word], str + i, charset);
+			i += j;
+			word++;
+		}
+	}
 }
 
 char	**ft_split(char *str, char *charset)
 {
-	char **strs;
-	char *new_str;
-	char *sec_str;
-	int i;
-	
-	i = -1;
-	new_str = ft_strstr(str, charset);
-	while (new_str != 0)
-	{
-		sec_str = ft_strstr(new_str, charset);
-		if (sec_str != 0)
-			strs[++i] = &new_str;
-		new_str = sec_str;
-	}
-	return (strs);
+	char	**array;
+	int		words;
+
+	words = count_words_for_mall(str, charset);
+	array = (char **)malloc(sizeof(char *) * (words + 1));
+	array[words] = 0;
+	split(array, str, charset);
+	return (array);
 }
 
-int main(void)
+/* int	main(void)
 {
-
-	char str1[] = "01234567834";
-	char str2[] = "34";
-	char **istr = ft_split(str1, str2);
-	if (istr != 0)
-		printf("str %s", istr[0]);
-	else 
-		printf("lol");
-	return 0;
-}
+	char	**str;
+	int		i;
+	
+	str = ft_split("kek&lol&trol", "&");
+	i = 0;
+	while (i < 3)
+	{
+		printf("%s\n", str[i]);
+		++i;
+	}
+	
+} */
